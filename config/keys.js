ddc8a24590363831ff38343432eac37de851b881
ziroock <zaprin.ignatiev@gmail.com>
2020-06-04 21:31:59 -0700

    Connected atlas mongo db successfully

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://ziroock:TOyfSSI17ai7SVoq@mango-media-00-dknut.mongodb.net/mango-media-00?retryWrites=true&w=majority',
+}
+//username: ziroock
+// password: TOyfSSI17ai7SVoq
+
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connection.once('open', () => {
+   console.log("MongoDB connection established successfully!");
+});
 
 app.get('/', (req, res) => {
     res.send({hi: 'there'});

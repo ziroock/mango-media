89a5d0530477fa534567b70423be56c94de5f33e
ziroock <zaprin.ignatiev@gmail.com>
2020-06-04 23:08:57 -0700

    Refactored authRoutes.js and index.js a bit.

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const keys = require('./config/keys');
-
 require('./models/User');
+
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true
+});
 
 const app = express();
 app.use(express.json());
-
-mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true});
-
-mongoose.connection.once('open', () => {
-   console.log("MongoDB connection established successfully!");
-});
 
 app.get('/', (req, res) => {
     res.send({hi: 'there'});
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,23 +2,22 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
 module.exports = app => {
-
     app.get('/api/register', async (req, res) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        res.status(422).send('Please enter username and password!');
-        return null;
-    }
+        const { email, password } = req.body;
+        if (!email || !password) {
+            res.status(422).send('Please enter username and password!');
+            return null;
+        }
 
-    const user = new User({ email: email, password: password });
+        const user = new User({ email: email, password: password });
 
-    try {
-        await user.save();
-        res.send('Registration successful!');
-    } catch(err) {
-        res.status(409).send(
-            (err.name === 'MongoError' && err.code === 11000) ? 'Email is already in use!' : err.message
-        );
-    }
-});
+        try {
+            await user.save();
+            res.send('Registration successful!');
+        } catch(err) {
+            res.status(409).send(
+                (err.name === 'MongoError' && err.code === 11000) ? 'Email is already in use!' : err.message
+            );
+        }
+    });
 };

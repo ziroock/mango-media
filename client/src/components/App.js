775f3e7daa80432dd6790fbbdaa99fc07af5b55e
ziroock <zaprin.ignatiev@gmail.com>
2020-06-07 19:24:39 -0700

    Connected index.js and App.js

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      Hi There!
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from "redux";
+
+
+import App from './components/App';
+
+ReactDOM.render(<App />, document.querySelector('#root'));

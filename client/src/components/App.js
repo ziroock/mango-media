c51d8ed3b69100720f1e38441f7ce6b02fbeb454
ziroock <zaprin.ignatiev@gmail.com>
2020-06-07 22:02:41 -0700

    Hooked up react-router-dom and finished basic redux setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Home = () => <h2>Home</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const Login = () => <h2>Login</h2>;
 
 const App = () => {
   return (
     <div className="App">
-      Hi There!
+        <BrowserRouter>
+            <div>
+                <Header />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/dashboard" component={Dashboard}/>
+                <Route path="/login" component={Login}/>
+            </div>
+        </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;

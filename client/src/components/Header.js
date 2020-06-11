7ffc2ba7b75a6d38e7cf0faa4d9b41de90251734
ziroock <zaprin.ignatiev@gmail.com>
2020-06-11 15:30:54 -0700

    Got access to the authReducer state inside Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
     render() {
+        // console.log(this.props);
         return (
             <nav>
                 <div className="nav-wrapper light-blue accent-3">
                     <div style={{margin: '0 1em'}}>
-                    <a href="#" className="brand-logo">Mango-Media</a>
-                    <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a href="#">Login</a></li>
-                        <li><a href="#">Register</a></li>
-                    </ul>
+                        <a href="#" className="brand-logo">Mango</a>
+                        <ul id="nav-mobile" className="right hide-on-med-and-down">
+                            <li><a href="#">Login</a></li>
+                            <li><a href="#">Register</a></li>
+                        </ul>
                     </div>
                 </div>
             </nav>
@@ -18,4 +20,9 @@ class Header extends Component {
     }
 }
 
-export default Header;
+// Gets called with the entire state statement our of the reduxStore
+function mapStateToProps(state) {
+    return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(Header);

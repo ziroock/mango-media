30889b2cfd6e87401b9d079252186cfb376f9e25
ziroock <zaprin.ignatiev@gmail.com>
2020-06-11 15:05:00 -0700

    Added ReduxThunk and axios to make ajax requests to the back end and keep the state in redux for current_user

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 const Home = () => <h2>Home</h2>;
@@ -7,20 +9,26 @@ const Dashboard = () => <h2>Dashboard</h2>;
 const Login = () => <h2>Login</h2>;
 const Register = () => <h2>Register</h2>;
 
-const App = () => {
-  return (
-    <div className="App">
-        <BrowserRouter>
-            <div>
-                <Header />
-                <Route exact path="/" component={Home} />
-                <Route exact path="/dashboard" component={Dashboard}/>
-                <Route path="/login" component={Login}/>
-                <Route path="/register" component={Register} />
-            </div>
-        </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
 
-export default App;
+    render() {
+        return (
+            <div className="App">
+                <BrowserRouter>
+                    <div>
+                        <Header/>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/dashboard" component={Dashboard}/>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/register" component={Register}/>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
+}
+
+export default connect(null, actions)(App);

2ae97204391b8d5a003e5fbd055c8b0b745f8f1c
ziroock <zaprin.ignatiev@gmail.com>
2020-06-11 15:15:50 -0700

    Added some code to authReducer so we know what is happening with the user

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,14 @@
-export default function(state = {}, action) {
-    console.log(action);
+import { FETCH_USER } from '../actions/types';
+
+// if state null ( don't know what is happening may be slow internet ... )
+// action.payload || false returns the user_id or if '' returns false
+
+// TODO: Need to make sure that only the user_id or username is what is seen
+export default function(state = null, action) {
+    // console.log(action);
     switch (action.type) {
+        case FETCH_USER:
+            return action.payload || false;
         default:
             return state;
     }

f922b4fc15aa6cd0debbca02218de371ce48d37b
ziroock <zaprin.ignatiev@gmail.com>
2020-06-25 00:43:31 -0700

    Added logic inside ProfileDashboard.js to request the user to to sing in in order to see dashboard

--- client/src/components/dashboard/ProfileDashboard.js
+++ client/src/components/dashboard/ProfileDashboard.js
@@ -1,19 +1,34 @@
 // This contains the full User Profile
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import ProfileCover from "./ProfileCover";
 import ProfilePost from "./ProfilePost";
 //TODO: Good for now, but need to make scalable keeping the same resolution
 
 class ProfileDashboard extends Component {
+    renderContent() {
+        switch(this.props.auth) {
+            case null:
+                return <h2>Please Sign In to access dashboard!</h2>;
+            case false:
+                return <h2>Please Sign In to access dashboard!</h2>;
+            default:
+                return (
+                    <div className="container green lighten-4">
+                        <ProfileCover/>
+                        <ProfilePost/>
+                    </div>
+                );
+        }
+    }
+
     render() {
-        return (
-            <div className="container green lighten-4">
-                <ProfileCover/>
-                <ProfilePost/>
-            </div>
-        )
+        return this.renderContent();
     }
 }
+function mapStateToProps(state) {
+    return { auth: state.auth };
+}
 
-export default ProfileDashboard;
+export default connect(mapStateToProps)(ProfileDashboard);

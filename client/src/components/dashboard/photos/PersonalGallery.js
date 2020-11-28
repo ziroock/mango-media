d1047da9b0ea9a804e7fe348aae9da3676317dca
ziroock <zaprin.ignatiev@gmail.com>
2020-11-27 17:36:11 -0800

    Starting clean

--- client/src/components/dashboard/photos/PersonalGallery.js
+++ client/src/components/dashboard/photos/PersonalGallery.js
@@ -1,48 +1,19 @@
-//https://docs.imgix.com/tutorials/react-responsive-image-gallery
-//https://codesandbox.io/s/5vn3lvz2n4?file=/index.js:0-1104
-
-/**
- * The Picture Gallery is now working. I need to fix
- * the full screen, maybe give it z index or somehow
- * separate from header. Also still need to connect to
- * db and figure out picture storage, dynamic loading etc...
- * **/
+//https://github.com/iamshaunjp/firegram/tree/final-files
 
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import Gallery from "react-photo-gallery";
-import Carousel, { Modal, ModalGateway } from "react-images";
 import { fetchPicture } from '../../../actions';
 
 class PersonalGallery extends Component{
     constructor(props) {
         super(props);
-
-        this.state = {
-            currentImage: 0,
-            viewerIsOpen: false,
-        }
-        // console.log(props);
-        // console.log( "KOKO: " + props.photo);
-        this.closeLightbox = this.closeLightbox.bind(this);
-        this.openLightbox = this.openLightbox.bind(this);
     }
 
     componentDidMount() {
         this.props.fetchPicture( {userId: this.props.match.params.userId} );
         //console.log( this.props.match.params.userId );
     }
-
-    openLightbox = (event, { photo, index }) => {
-        this.setState({ currentImage: index });
-        this.setState({ viewerIsOpen: true});
-        //console.log('Clicked!');
-    };
-
-    closeLightbox = () => {
-        this.setState({ currentImage: 0 });
-        this.setState({ viewerIsOpen: false});
-    };
 
     Title() {
         return (

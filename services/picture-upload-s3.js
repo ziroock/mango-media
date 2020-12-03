36254ea6ef45bbfbc1a609b1af0045499ec21367
ziroock <zaprin.ignatiev@gmail.com>
2020-12-02 18:34:54 -0800

    Small fix to force images to be uploaded publicly and also insted aof download just shown on the web.

--- services/picture-upload-s3.js
+++ services/picture-upload-s3.js
@@ -4,28 +4,30 @@ const multerS3 = require('multer-s3');
 const keys = require('../config/keys');
 
 aws.config.update({
-    secretAccessKey: keys.AWS_ACCESS_KEY_ID,
-    accessKeyId: keys.AWS_SECRET_ACCESS_KEY_ID,
+    secretAccessKey: keys.AWS_SECRET_ACCESS_KEY_ID,
+    accessKeyId: keys.AWS_ACCESS_KEY_ID,
     region: keys.AWS_REGION
 });
 
 
 const s3 = new aws.S3({ /* ... */ })
 
-// const upload = multer({
-//     storage: multerS3({
-//         s3: s3,
-//         bucket: 'mango-media-album',
-//         metadata: function (req, file, cb) {
-//             cb(null, {fieldName: "TESTING_METADATA"});
-//         },
-//         key: function (req, file, cb) {
-//             cb(null, Date.now().toString())
-//         }
-//     })
-// });
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'mango-media-album',
+        acl: 'public-read',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        metadata: function (req, file, cb) {
+            cb(null, {fieldName: "TESTING_METADATA"});
+        },
+        key: function (req, file, cb) {
+            cb(null, Date.now().toString())
+        }
+    })
+});
 
-const upload = multer({ dest: 'uploads/' });
+// const upload = multer({ dest: 'uploads/' });
 
 
 module.exports = upload;

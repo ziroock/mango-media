2ca9c97692cb715f948263e79496b32b5a12bea4
ziroock <zaprin.ignatiev@gmail.com>
2020-12-26 22:34:45 -0800

    Added new gallery, way better!

--- client/src/components/dashboard/photos/PersonalGallery.js
+++ client/src/components/dashboard/photos/PersonalGallery.js
@@ -6,12 +6,61 @@ import { fetchPicture } from '../../../actions';
 import UploadPicModal from "./UploadPicModal";
 import PictureGrid from './PictureGrid';
 import { isPersonal } from "../../../utils/mango.utils";
+import Gallery from '../../../utils/GalleryAPI/Gallery';
+import jsonp from 'jsonp';
 
 class PersonalGallery extends Component{
+    constructor() {
+        super();
+        this.state = { width: -1 };
+        this.loadPhotos = this.loadPhotos.bind(this);
+    }
 
     componentDidMount() {
         this.props.fetchPicture( {userId: this.props.match.params.userId} );
+        this.loadPhotos();
     }
+
+    loadPhotos() {
+        const urlParams = {
+            api_key: '455b5e2fa6b951f9b9ab58a86d5e1f8a',
+            photoset_id: '72157708141247864',
+            user_id: '146659101@N08',
+            format: 'json',
+            per_page: '120',
+            extras: 'url_m,url_c,url_l,url_h,url_o',
+        };
+
+        let url = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos';
+        url = Object.keys(urlParams).reduce((acc, item) => {
+            return acc + '&' + item + '=' + urlParams[item];
+        }, url);
+
+        jsonp(url, { name: 'jsonFlickrApi' }, (err, data) => {
+            let photos = data.photoset.photo.map(item => {
+                let aspectRatio = parseFloat(item.width_o / item.height_o);
+                return {
+                    src: item.url_l,
+                    width: parseInt(item.width_o),
+                    height: parseInt(item.height_o),
+                    title: item.title,
+                    alt: item.title,
+                    key: item.id,
+                    srcSet: [
+                        `${item.url_m} ${item.width_m}w`,
+                        `${item.url_c} ${item.width_c}w`,
+                        `${item.url_l} ${item.width_l}w`,
+                        `${item.url_h} ${item.width_h}w`,
+                    ],
+                    sizes: '(min-width: 480px) 50vw, (min-width: 1024px) 33.3vw, 100vw',
+                };
+            });
+            this.setState({
+                photos: this.state.photos ? this.state.photos.concat(photos) : photos,
+            });
+        });
+    }
+
 
     Title() {
         return (
@@ -40,7 +89,15 @@ class PersonalGallery extends Component{
         }
         return null;
     }
+    renderBatBoiko() {
+            if(this.state.photos) {
+                return (
+                    <Gallery photos={this.state.photos.slice(60, 75)}/>
 
+                );
+            }
+            return null;
+    }
 
     renderGallery() {
         return(
@@ -48,6 +105,7 @@ class PersonalGallery extends Component{
                 {this.Title()}
                 {this.renderUpload()}
                 {this.renderGrid()}
+                {this.renderBatBoiko()}
             </div>
         )
     }

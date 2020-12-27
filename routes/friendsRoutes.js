4e47cc67e19ad41bca99081bd7e8fe43166361b0
ziroock <zaprin.ignatiev@gmail.com>
2020-12-27 02:34:06 -0800

    Plan on how to tackle friendsRoutes.js

---
+++ routes/friendsRoutes.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const requireLogin = require('../middleware/requireLogin');
+
+
+
+module.exports = app => {
+    //1. Start by doing follow
+    //  - follow
+    //  - return follow list
+    //  - num followers & num following
+    //2. Start Clique ...
+}

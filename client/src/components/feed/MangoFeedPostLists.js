6c75dc2b07b3e4fe85f0085668f2f4c6d1c54e9e
ziroock <zaprin.ignatiev@gmail.com>
2021-01-05 14:31:49 -0800

    created show more button for comments. Need to create show less as well. and then only show show more or show less...

--- client/src/components/feed/MangoFeedPostLists.js
+++ client/src/components/feed/MangoFeedPostLists.js
@@ -18,10 +18,46 @@ import { fetchFeed } from '../../actions';
 *     <ProfilePostMenu/> handles edits and deletes based on the info received.
 * */
 
+/** TODO TODO TODO TODO TODO
+ *
+ *  - make mango-post-main component
+ *  - crate a toggle function that opens and closes more comments
+ *      - make sure only one show more open at a time !!!
+ *        this means that when a user has an open show more comments on one post
+ *        when he opens another one the already opened one needs to close!
+ *  - finish show more working properly  aka scrollable
+ *  - make a function that auto generates n number of comments
+ *  - add ES lint from the gallery project
+ *
+ * **/
+
 
 class MangoFeedPostLists extends Component {
     componentDidMount() {
         this.props.fetchFeed();
+    }
+
+
+    // function that auto generates n number of comments
+    genComments = (n) => {
+        let commentsBody = [];
+        for(let i = 0; i < n; i++) {
+            let str = "This is comment " + i + " and I like it";
+            commentsBody.push(str)
+        }
+        if(commentsBody.length > 0) {
+            return commentsBody.map((body) => {
+                return(
+                    <div className="post-reply-box">
+                        <div className="post-reply-avatar"></div>
+                        <div className="post-reply-text-box">
+                            <textarea disabled className="text-area" value={body}/>
+                        </div>
+                    </div>
+                )
+            })
+        }
+        return null;
     }
 
     //TODO: Add conditional to change from military time to am/pm because people don't know military time
@@ -59,23 +95,14 @@ class MangoFeedPostLists extends Component {
                             <textarea disabled className="text-area" value={post.body}/>
                         </div>
                         <div id={postReplyID} className="post-reply-container">
-                            <div className="post-reply-box">
-                                <div className="post-reply-avatar"></div>
-                                <div className="post-reply-text-box">
-                                    <textarea disabled className="text-area" value={post.body}/>
-                                </div>
-                            </div>
-                            <div className="post-reply-box">
-                                <div className="post-reply-avatar"></div>
-                                <div className="post-reply-text-box">
-                                    <textarea disabled className="text-area" value={post.body}/>
-                                </div>
-                            </div>
-                            <button type="button" onClick={ () => {
-                                document.getElementById(postId).style.height = '400px';
-                                document.getElementById(postReplyID).style.height = '66%';
-                            }}>Click Me!</button>
+                            {this.genComments(5)}
                         </div>
+                        <button className="show-more" type="button" onClick={ () => {
+                            document.getElementById(postId).style.height = '400px';
+                            document.getElementById(postReplyID).style.height = '66%';
+                            document.getElementById(postReplyID).style.overflowY = 'auto';
+                            document.getElementById(postReplyID).style.overflowX = 'hidden';
+                        }}>show more</button>
                     </div>
                 );
             });

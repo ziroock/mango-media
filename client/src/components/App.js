14e99bda9129213e033f8bdbdb314eb0abf31305
ziroock <zaprin.ignatiev@gmail.com>
2021-01-28 18:31:15 -0800

    Integrated new components, need to remove some old styles and libraries to fix the css properly

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -15,7 +15,9 @@ import '../style/style.css';
 import '../style/style.personalGallery.css';
 import '../style/style.profileDashboard.css';
 import '../style/style.mangoPost.css';
-import '../style/iphone-11-pro-x-login.css';
+// import '../style/iphone-11-pro-x-login.css';
+import '../style/mango-login.css';
+import '../style/mango.css';
 
 /*
  * The App Component is the what puts everything together! It is where the
